69b4893274268bc42a716c9d491f3cc37062fa58
unknown <t.panek@ZIT-M0305.AD.KWSA.PL>
2013-12-23 09:25:10 +0100

    add user

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Esnu::Application.config.secret_key_base = 'e3b513af3bcb130bba3db38de3c2c8eba5c7a8a1af07d22ee71c0d6c2b25c91d35e91a28273f980956f6c6fa866127877746dc2e3cc0f435542451e95126463f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Esnu::Application.config.session_store :cookie_store, key: '_esnu_session'
